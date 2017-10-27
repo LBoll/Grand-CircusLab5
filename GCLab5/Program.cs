368f99435a6494f1cc81636104bada3b12eaedfb
LBoll <lboll582@gmail.com>
2017-10-27 10:04:17 -0400

    Commiting update

--- GCLab5/Program.cs
+++ GCLab5/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GCLab5
 {
@@ -10,60 +6,93 @@ namespace GCLab5
     {
         static void Main(string[] args)
         {
-            bool repeat = true;
-            string userName;
-            while (repeat)
+            Console.WriteLine("Welcome to the Grand Circus Casino! What is your name? ");
+            string userName = Console.ReadLine();
+
+            while (true) //to make sure the user wants to play or exit 
             {
-                Console.WriteLine("Welcome to the Grand Circus Casino! Let's start with your name: ");
-                userName = Console.ReadLine();
-
-                Console.Write($"{userName}, would you like to roll the dice? (y/n) ");
-
-                bool success = true;
-                while (success)
+                //Ask user if they want to play
+                Console.WriteLine($"{userName} would you like to roll some dice (y/n)? ");
+                bool Casino = GetYesorNo();
+                if (Casino == false)
                 {
-                    string answer = Console.ReadLine();
-                    if (answer == "y" || answer == "Y" )
-                    {
-                        repeat = true;
-                        success = false;
-                    }
-                    else if (answer == "n" || answer == "N")
-                    {
-                        Console.Write($"Come on, {userName}! Gambling is fun!(: ");
-                        success = true;
-
-                        string answer2 = Console.ReadLine();
-                        if (answer2 == "y" || answer2 == "Y")
-                        {
-                            repeat = true;
-                            success = false;
-                        }
-                        else if (answer2 == "n" || answer2 == "N" || answer2 == "no" || answer2 == "No")
-                        {
-                            Console.WriteLine("I see it was a waste of time to even ask for your name. Good riddance!\n" );
-                            success = true;
-                            return;
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Come on {userName}, it is a yes or no question. ");
-                            Console.WriteLine("Please enter a valid input! ");
-                            success = true;
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Please enter a valid input! ");
-                        success = true;
-                    }
+                    return;
                 }
 
-                Console.WriteLine($"Let us begin! {userName}, how many sides does the dice have? ");
-                return;
+                while (Casino)
+                {
+                    //get prompt user for input for sides on dice
+                    Console.Write("Entering the Dice Roller!\nPlease enter the number of sides on your dice: ");
 
+                    bool repeat = true;
+
+                    //get valid input
+                    int input = GetInt(userName);
+
+                    while (repeat)
+                    {
+
+                        //Loop for rolling selected dice must exit to choose new dice
+                        Random generator = new Random();
+                        int diceRoll1 = generator.Next(input) + 1;
+                        int diceRoll2 = generator.Next(input) + 1;
+                        Console.WriteLine(diceRoll1 + "\n");
+                        Console.WriteLine(diceRoll2 + "\n");
+
+                        Console.WriteLine($"You rolled a {diceRoll1} and a {diceRoll2} on die with {input} sides!");
+
+                        //Prompt user for another roll
+                        Console.Write($"{userName} you like to roll the same dice again (y/n)? ");
+                        repeat = GetYesorNo();
+                        Casino = false;
+                    }
+
+                }
             }
-            
+        }
+
+        private static bool GetYesorNo()
+        {
+            bool valid = true;
+            bool repeat = true;
+            while (valid)
+            {
+                string answer = Console.ReadLine().ToLower();
+                if (answer == "y" || answer == "yes")
+                {
+                    valid = false;
+                    repeat = true;
+                }
+                else if (answer == "n" || answer == "no")
+
+                {
+                    valid = false;
+                    repeat = false;
+                }
+                else
+                {
+                    Console.Write("Did not enter a valid input. Please enter (y/n): ");
+                }
+            }
+            return repeat;
+        }
+
+        private static int GetInt(string name)
+        {
+            bool valid = false;
+            int input = 0;
+
+            while (!valid)
+            {
+
+                string sides = Console.ReadLine();
+                valid = int.TryParse(sides, out input);
+            }
+            if (!valid)
+            {
+                Console.WriteLine($"{name}... You didn't enter valid input. Try again: ");
+            }
+            return input;
         }
     }
 }
